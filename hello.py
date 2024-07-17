6247ae7044d48789cdd01123e3bc32b3d6df229d
habibimazin <mazinsameersaleh@gmail.com>
2024-07-17 15:44:59 -0700

    push

---
+++ hello.py
@@ -0,0 +1,10 @@
+name = input("Hello. What is your name? ")
+print("It's nice to meet you, " + name + ". How old are you?")
+
+age = input()
+print("I see that you are still quite young at only", age , ".")
+
+location = input("Where do you live?")
+print(" Wow! I've always wanted to go to " + location + ". Thanks for chatting with me. Bye! ")
+
+
