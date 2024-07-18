4fba485f866894294c3358fd90630a1170b75bcb
habibimazin <mazinsameersaleh@gmail.com>
2024-07-17 21:00:07 -0700

    first commit and push

--- hello.py
+++ hello.py
@@ -7,4 +7,4 @@ print("I see that you are still quite young at only", age , ".")
 location = input("Where do you live?")
 print(" Wow! I've always wanted to go to " + location + ". Thanks for chatting with me. Bye! ")
 
-
+##
